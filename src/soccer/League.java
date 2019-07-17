52f8a0dd85308950f365d43a41583482c6c5d315
ayolicias <ziyandandlamafa47@gmail.com>
2019-07-17 15:50:13 +0200

    commiting League class

--- src/soccer/League.java
+++ src/soccer/League.java
@@ -17,13 +17,30 @@ public class League {
         Player[] thePlayers = {player1, player2, player3};
 
         Team team1 = new Team();
-        team1.teamName = "the Greens";
-
+        team1.teamName = "The Greens";
         team1.playerArray = thePlayers;
+
+
+        Team team2 = new Team();
+        team2.teamName = "The Red";
+
+        team2.playerArray = new Player[3];
+        team2.playerArray[0] = new Player();
+        team2.playerArray[0].PlayerName = "Robert Service";
+
+        team2.playerArray[1] = new Player();
+        team2.playerArray[1].PlayerName = "Robbie Burns";
+
+        team2.playerArray[2] = new Player();
+        team2.playerArray[2].PlayerName = "Raphael Sabatini";
+
         for (Player thePlayer: team1.playerArray){
         System.out.println(thePlayer.PlayerName);
         }
 
+        for (Player thePlayer: team2.playerArray){
+            System.out.println(thePlayer.PlayerName);
+        }
 
     }
 }
