bee4361a748795bb6c91dd1c5558b43e707f1c6e
ayolicias <ziyandandlamafa47@gmail.com>
2019-07-23 10:21:38 +0200

    encapsulation added

--- src/soccer/League.java
+++ src/soccer/League.java
@@ -2,54 +2,87 @@ package soccer;
 
 public class League {
 
+    private static String team1;
+    private static String team2;
+
     public static void main( String[] args ) {
         // To do code logic here
 
         Player player1 = new Player();
 
-        player1.PlayerName = "George Elliot";
+        player1.setPlayerName("George Elliot");
         Player player2 = new Player();
-        player2.PlayerName = "Graham Green";
+        player2.setPlayerName("Graham Green");
         Player player3 = new Player();
-        player3.PlayerName = "Geoffrey Chaucer";
+        player3.setPlayerName("Geoffrey Chaucer");
 
 
         Player[] thePlayers = {player1, player2, player3};
 
         Team team1 = new Team();
-        team1.teamName = "The Greens";
-        team1.playerArray = thePlayers;
+        team1.setTeamName("The Greens");
+        team1.setPlayerArray(thePlayers);
 
 
         Team team2 = new Team();
-        team2.teamName = "The Red";
+        team2.setTeamName("The Red");
 
-        team2.playerArray = new Player[3];
-        team2.playerArray[0] = new Player();
-        team2.playerArray[0].PlayerName = "Robert Service";
+        team2.setPlayerArray(new Player[3]);
+        team2.getPlayerArray()[0] = new Player();
+        team2.getPlayerArray()[0].setPlayerName("Robert Service");
 
-        team2.playerArray[1] = new Player();
-        team2.playerArray[1].PlayerName = "Robbie Burns";
+        team2.getPlayerArray()[1] = new Player();
+        team2.getPlayerArray()[1].setPlayerName("Robbie Burns");
 
-        team2.playerArray[2] = new Player();
-        team2.playerArray[2].PlayerName = "Raphael Sabatini";
+        team2.getPlayerArray()[2] = new Player();
+        team2.getPlayerArray()[2].setPlayerName("Raphael Sabatini");
 
         Game currGame = new Game();
-        currGame.homeTeam = team1;
-        currGame.awayTeam = team2;
+        currGame.setHomeTeam(team1);
+        currGame.setAwayTeam(team2);
 
         Goal goal1 = new Goal();
-        goal1.thePlayer = currGame.homeTeam.playerArray[2];
-        goal1.theTeam = currGame.homeTeam;
-        goal1.theTime = 55;
+        goal1.setThePlayer(currGame.getHomeTeam().getPlayerArray()[2]);
+        goal1.setTheTeam(currGame.getHomeTeam());
+        goal1.setTheTime(55);
 
         Goal[] theGoals = {goal1};
-        currGame.goals = theGoals;
+        currGame.setGoals(theGoals);
 
         System.out.println("Goal scored after " +
-                currGame.goals[0].theTime + " mins by " +
-                currGame.goals[0].thePlayer.playerName + " of " +
-                currGame.goals[0].theTeam.teamName);
+                currGame.getGoals()[0].getTheTime() + " mins by " +
+                currGame.getGoals()[0].getThePlayer().getPlayerName() + " of " +
+                currGame.getGoals()[0].getTheTeam().getTeamName());
+
+        for (Player theplayer: team2.getPlayerArray()){
+                if (theplayer.getPlayerName().matches(".*Sab.*")){
+                    theplayer.getPlayerName().split(" ");
+                    String s = theplayer.getPlayerName().split(" ")[1];
+                    System.out.println("Found " + theplayer.getPlayerName());
+                    System.out.println("Last Name is " + theplayer.getPlayerName().split(" ")[1]);
+                }
+        }
+
+            for (Player theplayer: team1.getPlayerArray()){
+                String name[] = theplayer.getPlayerName().split(" ");
+                StringBuilder familyNameFirst = new StringBuilder();
+                familyNameFirst.append(name [1]);
+                familyNameFirst.append(", ");
+                familyNameFirst.append(name[0]);
+                familyNameFirst.append(familyNameFirst);
+
+                familyNameFirst.delete(0, familyNameFirst.length());
+
+
+        }
 
     }
+
+        public static Team[] createTeams() {
+
+            Player player1 = new Player("George Elliot");
+//            String[] theTeams = {team1, team2};
+            return createTeams();
+        }
+
 }
--- src/soccer/Player.java
+++ src/soccer/Player.java
@@ -2,7 +2,23 @@ package soccer;
 
 public class Player {
 
-public String PlayerName;
+private String PlayerName;
+private String playerName;
 
-    public String playerName;
+    //adding constructor
+    public Player(String PlayerName){
+        PlayerName = playerName;
+    }
+    //Add constructor with no arguments
+    public Player(){
+
+    }
+
+    public String getPlayerName( ) {
+        return PlayerName;
+    }
+
+    public void setPlayerName( String playerName ) {
+        PlayerName = playerName;
+    }
 }
